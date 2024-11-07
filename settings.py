5b3ff9271041fc44bfae679fc11f94a21a7a1b38
Martin Aghajanian <martin.aghajanian@gmail.com>
2024-11-07 11:14:53 +0400

    added settings configurations

---
+++ settings.py
@@ -0,0 +1,16 @@
+# Grid settings
+GRID_SIZE = 20        # Size of each grid square in pixels
+GRID_WIDTH = 20       # Number of columns in the grid
+GRID_HEIGHT = 20      # Number of rows in the grid
+SCREEN_WIDTH = GRID_WIDTH * GRID_SIZE
+SCREEN_HEIGHT = GRID_HEIGHT * GRID_SIZE
+
+# Colors (RGB)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)    # Snake color
+RED = (255, 0, 0)      # Fruit color
+BLUE = (0, 0, 255)     # Wall color
+
+# Game settings
+FPS = 10               # Frames per second (game speed)
